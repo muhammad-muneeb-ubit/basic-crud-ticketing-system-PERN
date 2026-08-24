9f23712797f334f2dcd902853992f391385ea982
Muneeb <Muhammad.Muneeb4@corp.ibexglobal.com>
2026-08-24 13:40:50 +0500

    'pg-hosted'

--- server/db/config.js
+++ server/db/config.js
@@ -1,15 +1,26 @@
 import { Pool } from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+let pool;
 
-// console.log("Database connection established successfully.");
+if (process.env.DATABASE_URL) {
+  // Production database
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  // Local database
+  pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  });
+}
+
 export default pool;
